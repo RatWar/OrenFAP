cb8599a077f025c225a320cfb001df6e3f6b63c3
Rais <8Ud-9xV-t75-VBq>
2023-06-13 17:13:00 +0500

    invent-13062023

--- app/src/main/java/com/besaba/anvarov/orentsd/activity/InventActivity.kt
+++ app/src/main/java/com/besaba/anvarov/orentsd/activity/InventActivity.kt
@@ -2,11 +2,37 @@ package com.besaba.anvarov.orentsd.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.besaba.anvarov.orentsd.R
 
 class InventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_invent)
+        val btnRemains = findViewById<Button>(R.id.btnRemains)
+        btnRemains.setOnClickListener { loadRemains() }
+        val btnSaveFTP = findViewById<Button>(R.id.btnSaveFTP)
+        btnSaveFTP.setOnClickListener { saveInvent() }
+        val tvDoc = findViewById<TextView>(R.id.tvDoc)
+        tvDoc.setOnClickListener { openDoc() }
+        val tvReport = findViewById<TextView>(R.id.tvReport)
+        tvReport.setOnClickListener { openReport() }
+    }
+
+    private fun loadRemains() {
+
+    }
+
+    private fun saveInvent() {
+
+    }
+
+    private fun openDoc() {
+
+    }
+
+    private fun openReport() {
+
     }
 }
---
+++ app/src/main/java/com/besaba/anvarov/orentsd/activity/ListFAPActivity.kt
@@ -0,0 +1,12 @@
+package com.besaba.anvarov.orentsd.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.besaba.anvarov.orentsd.R
+
+class ListFAPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_fapactivity)
+    }
+}
